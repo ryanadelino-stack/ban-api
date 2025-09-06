4455ece6884e3a699c889d7296b22b5022fcdba6
ryanadelino-stack <ryan.adelino@gmail.com>
2025-09-06 00:06:49 -0300

    chore: remover CorsConfig duplicada (em src/test)

--- src/test/java/bf/tools/ban_api/BanApiApplicationTests.java
+++ src/test/java/bf/tools/ban_api/BanApiApplicationTests.java
@@ -1,13 +1,11 @@
 package bf.tools.ban_api;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class BanApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+@SpringBootApplication(scanBasePackages = { "bf.tools" }) // <- chave do conserto
+public class BanApiApplicationTests {
+  public static void main(String[] args) {
+    SpringApplication.run(BanApiApplication.class, args);
+  }
 }
