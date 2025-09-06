7dcc48250909f7085636eb77f06045eb0bb71660
ryanadelino-stack <ryan.adelino@gmail.com>
2025-09-05 23:27:13 -0300

    fix(scan): incluir bf.tools no component scan

--- src/main/java/bf/tools/ban_api/BanApiApplication.java
+++ src/main/java/bf/tools/ban_api/BanApiApplication.java
@@ -3,11 +3,10 @@ package bf.tools.ban_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "bf.tools" })
 public class BanApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BanApiApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(BanApiApplication.class, args);
+  }
 }
